9203a6af2412841020f01ba2d0d080ea66b0b8cf
Camusama <a1012401749@163.com>
2018-06-06 22:28:52 +0800

    day4

---
+++ src/common/gallary/Gallary.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container">
+    <div class="wrapper">
+      <swiper :options="swiperOptions">
+      <swiper-slide>
+        <img src="http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_800x800_70debc93.jpg" class="gallary-img">
+      </swiper-slide>
+      <swiper-slide>
+        <img src="http://img1.qunarzz.com/sight/p0/1709/76/7691528bc7d7ad3ca3.img.png_800x800_9ef05ee7.png" class="gallary-img">
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'commonGallary',
+  data() {
+    return {
+      swiperOptions: {
+        pagination:'.swiper-pagination',
+        paginationType: 'fraction',
+        observeParents: true,
+        observer: true
+      }
+    }
+  }
+}
+</script>
+
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .container
+    display:flex
+    flex-direction:column
+    justify-content:center
+    position:fixed
+    left:0
+    right:0
+    top:0
+    bottom:0
+    background:#000
+    z-index:99
+    .wrapper
+      overflow:hidden
+      height:0
+      width:100%
+      padding-bottom:100%
+      .gallary-img
+        width:100%
+      .swiper-pagination
+        color: #fff
+        bottom: -1rem
+</style>
---
+++ src/pages/detail/Detail.vue
@@ -0,0 +1,20 @@
+<template>
+  <div >
+    <detail-banner></detail-banner>
+  </div>
+</template>
+
+<script>
+import DetailBanner from './components/banner'
+export default {
+  components : {
+    DetailBanner
+  }
+}
+</script>
+
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+
+</style>
